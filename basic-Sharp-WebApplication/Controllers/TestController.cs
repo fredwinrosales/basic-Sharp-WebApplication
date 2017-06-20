59502b72c7f050e6803e3885eb3088306116f846
Fredwin Rosales <fredwinrosales@gmail.com>
2017-06-20 11:43:35 -0400

    Primer commit

---
+++ basic-Sharp-WebApplication/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace basic_Sharp_WebApplication.Controllers
+{
+    public class TestController : Controller
+    {
+        public ActionResult HolaMundo()
+        {
+            return View();
+        }
+    }
+}
---
+++ basic-Sharp-WebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(basic_Sharp_WebApplication.Startup))]
+namespace basic_Sharp_WebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
